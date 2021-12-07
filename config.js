2583d67f3c152b0b2dc747475d58f4b046f76ea7
YooYoo72 <95701124+YooYoo72@users.noreply.github.com>
2021-12-07 22:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "https://docs.google.com/forms/u/0/",
+    introTitle: 'Gấu iu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Phải chăng Gấu đã iu em ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Gấu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm <33',
+    btnNo: 'Mơ à :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Gấu lại yêu em nhò <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Em bít mà 🥰. Yêu Gấu nhiều thật nhiều lun 😘😘',
+    messDesc: 'Noel này Gấu đi chơi với em nhá ^^',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
